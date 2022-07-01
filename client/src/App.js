8bcc15f2bf4087b48b12a9e3b514929ac1bcf539
ysaad01 <ysaad2325@gmail.com>
2022-06-30 21:01:57 -0400

    added ApolloProvider to App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
 import "./App.css";
+import { setContext } from "@apollo/client/link/context";
+import decode from "jwt-decode";
 import Landing from "./pages/Landing";
 import SearchLocation from "./pages/AddLoc";
+// import apollo
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Landing />
-      </header>
-      <SearchLocation />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <Landing />
+        </header>
+        <SearchLocation />
+      </div>
+    </ApolloProvider>
   );
 }
 
