ba02edbacad9b4be86b4a3826b294758948d2a8a
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-09 14:50:30 -0700

    more info for about.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,11 +9,19 @@ const About = () => {
         We've all experienced it, but have no fear! 
         The Delicious Connection is here!
       </p>
+
       <h3>
         What is the Delicious Connection
       </h3>
       <p>
-        The Delicious Connection is a random restaurant generator utilizing Yelp's API to allow us to suggest a restaurant on the most indecisive evenings.
+        The Delicious Connection is a random restaurant generator utilizing Yelp's API
+        to allow us to suggest a restaurant on the most indecisive evenings.
+      </p>
+      <p>
+        The generator will take your search location into account along with the price range
+        and distance you want to search in. The generator will provide the Yelp profile for the
+        restaurant, so that you can view the information and reviews before deciding to
+        make your Delicious Connection
       </p>
     </section>
   );
