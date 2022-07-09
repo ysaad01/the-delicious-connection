8e000e627e028587270f6399b5dddf15baf867b9
ysaad01 <ysaad2325@gmail.com>
2022-07-09 19:11:08 -0400

    edited css styling for reviews in carousel; changed bootstrap theme for easier view; removed API.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./bootstrap1.min.css";
+import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
