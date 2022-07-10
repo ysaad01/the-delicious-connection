cd9b2735b92ea72f7c054eddac364e26511afd5d
ysaad01 <ysaad2325@gmail.com>
2022-07-09 23:27:41 -0400

    moved typewriter to about.js component to clean up landing.js page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,8 +6,6 @@ import Carousel from "../components/Carousel1";
 import About from "../components/About";
 import SearchLocation from "./SearchLoc";
 
-import Typewriter from "typewriter-effect";
-
 const Landing = () => {
   return (
     <div>
@@ -24,20 +22,6 @@ const Landing = () => {
                 style={{ display: "block", width: 700, padding: 30 }}
               >
                 <About />
-                <h4>
-                  We've helped these&nbsp;
-                  <Typewriter
-                    options={{
-                      strings: ["hangry", "indecisive", "picky", "hesitant"],
-                      autoStart: true,
-                      loop: true,
-                    }}
-                    onInit={(typewriter) => {
-                      typewriter.start();
-                    }}
-                  />
-                  people
-                </h4>
                 <Carousel />
                 <h5>lets help you too</h5>
               </div>
